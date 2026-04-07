ebc8834a1a43cb8f04d56db3384dbbdee7f7929d
fbi2005z1000-cmyk <fbi2005z1000@gmail.com>
2026-04-07 10:55:05 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-module.exports = require('../backend/autoTrade');
+const { resolveAutoTradeProfile } = require('./autoTradeProfile');
+const { buildCapitalPlan } = require('./capitalPlan');
+const { evaluateSafetyRules } = require('./safetyRules');
+const { buildFuturesOrderPayload } = require('./binanceOrderPayload');
+const { evaluateIntent } = require('./intentModel');
+const { AutoTradeEngine } = require('./autoTradeEngine');
+
+module.exports = {
+  resolveAutoTradeProfile,
+  buildCapitalPlan,
+  evaluateSafetyRules,
+  buildFuturesOrderPayload,
+  evaluateIntent,
+  AutoTradeEngine,
+};
